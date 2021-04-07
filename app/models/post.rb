8c413d26e208d8d6de0b9a1530ff60050ac66db1
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-04-07 13:04:38 -0300

    Add validations to the posts and create the first post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3}
+  validates :content, presence: true, length: {minimum: 5}
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: {minimum: 2}
+
+  has_many :posts
 end
