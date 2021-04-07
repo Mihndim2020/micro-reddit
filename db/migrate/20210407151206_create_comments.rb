728c0ba7e7022e67128381d02464b7fa8f160daf
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-04-07 12:12:53 -0300

    Add the posts, user and comments models

---
+++ db/migrate/20210407151206_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.integer :user_id
+      t.integer :posts_id
+      t.integer :id
+
+      t.timestamps
+    end
+  end
+end
